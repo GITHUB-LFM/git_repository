b98399000aee7e23b3b79589fa8bfc99b5fcbb22
lifengming <1130025065@qq.com>
2018-06-27 14:26:46 +0800

    ''

---
+++ gamebox_billboard.sql
@@ -0,0 +1,14 @@
+
+-- ----------------------------
+-- Records of gamebox_billboard
+-- ----------------------------
+INSERT INTO `gamebox_billboard` VALUES ('1', '热度正在飙升', '1,27,29,11', '500', '0', '2018-06-21 10:28:21', '2018-06-21 10:28:21', '#ffc737', 'https://h5game.gametall.com/chatgame/app_res/game_res_upload/2018-06-19/1529395999135.png');
+INSERT INTO `gamebox_billboard` VALUES ('2', '经典还是经典', '26,9,5,7', '400', '0', '2018-06-21 10:28:22', '2018-06-21 10:28:22', '', '');
+INSERT INTO `gamebox_billboard` VALUES ('3', '手速大挑战', '10,16,8,6', '300', '0', '2018-06-21 10:28:22', '2018-06-21 10:28:22', '', '');
+INSERT INTO `gamebox_billboard` VALUES ('4', '最新发布', '30,12,2,3', '450', '0', '2018-06-21 10:28:22', '2018-06-21 10:28:22', '#f77623', 'https://h5game.gametall.com/chatgame/app_res/game_res_upload/2018-06-19/1529396098734.png');
+INSERT INTO `gamebox_billboard` VALUES ('5', '智力大挑战', '13,19,15,14', '100', '0', '2018-06-21 10:28:22', '2018-06-21 10:28:22', '', '');
+INSERT INTO `gamebox_billboard` VALUES ('6', '五花八门', '17,18,22,24', '50', '0', '2018-06-21 10:28:22', '2018-06-21 10:28:22', '', '');
+INSERT INTO `gamebox_billboard` VALUES ('7', '其它', '23,25,21,20', '20', '0', '2018-06-21 10:28:22', '2018-06-21 10:28:22', '', '');
+INSERT INTO `gamebox_billboard` VALUES ('8', '精选回顾', '28,31,33', '10', '0', '2018-06-21 10:28:22', '2018-06-21 10:28:22', '', '');
+
+test
